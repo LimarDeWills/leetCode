978c96c2209b630799c342b908030685b1cf7640
Mikhail Kharitonov <ma.haritonov@datagile.ru>
2022-09-01 07:51:15 +0300

    add solution for pivotIndex

---
+++ pivotIndex.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+//the pivot index
+
+//test cases:
+//{ 0 } = -1
+//{ 0, 1, 2 } = -1
+//{ 2, 1, 2 } = 1
+//{ 1, 7, 3, 6, 5, 6 } = 3
+//{ 6, 6, 6, 6, 0, 8, 8, 8, 0 } = 4
+
+int pivotIndex(std::vector<int>& nums)
+{
+    int sum = 0;
+    int leftSum = 0;
+    for (auto& el : nums)
+        sum += el;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (leftSum == (sum - leftSum - nums[i]))
+            return i;
+
+        leftSum += nums[i];
+    }
+
+
+    return -1;
+}
+
+int main()
+{
+    std::vector<int> nums = { 0, 1, 2 };
+
+    return pivotIndex(nums);
+}
